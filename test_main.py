80e2210a4b7f29b56364542324199aeb80761d7c
GTzx <1537282968@qq.com>
2023-09-25 19:08:56 +0800

    Add files via upload

--- test_main.py
+++ test_main.py
@@ -11,7 +11,7 @@ class TestArithmeticGenerator(unittest.TestCase):
         # 测试generate_number函数生成的数是否在合理范围内
         for _ in range(100):
             number = generate_number(10)
-            self.assertTrue(0 <= number <= 10)
+            self.assertTrue(0 <= number < 10)
 
     def test_generate_expression_equivalence(self):
         # 测试generate_expression函数生成的表达式是否有重复
@@ -34,17 +34,47 @@ class TestArithmeticGenerator(unittest.TestCase):
             result = convert_to_fraction(decimal)
             self.assertEqual(result, expected)
 
-    def test_is_answer_correct(self):
-        # 测试is_answer_correct函数是否正确判定答案是否正确
-        self.assertTrue(is_answer_correct("2 + 3", Fraction(5, 1), Fraction(5, 1)))
-        self.assertTrue(is_answer_correct("1/2 + 1/3", Fraction(5, 6), Fraction(5, 6)))
-        self.assertFalse(is_answer_correct("3 × 4", Fraction(12, 1), Fraction(7, 2)))
+    def test_convert_fraction(self):
+        # 测试convert_fraction函数是否正确将假分数转换为带分数
+        test_num1 = '5/3'  # 带分数为 1‘2/3
+        test_num2 = '34/11'  # 带分数为 3’1/11
+        test_num3 = '59/8'  # 带分数为 7‘3/8
+        test_num4 = '2612/315'  # 带分数为 8‘92/315
+        test_num5 = '383/40'  # 带分数为 9‘23/40
+        converted_test_num1 = '1‘2/3'
+        converted_test_num2 = '3’1/11'
+        converted_test_num3 = '7‘3/8'
+        converted_test_num4 = '8‘92/315'
+        converted_test_num5 = '9‘23/40'
+        if convert_fraction(test_num1) == converted_test_num1 and convert_fraction(test_num2) == converted_test_num2 and convert_fraction(test_num3) == converted_test_num3 and convert_fraction(test_num4) == converted_test_num4 and convert_fraction(test_num5) == converted_test_num5:
+            self.assertTrue(1)
+        else:
+            self.assertFalse(0)
+
+    def test_add_parentheses(self):
+        # 测试 add_parentheses 函数是否能正常为所有数加个括号
+        exp1 = "1 - 0 ÷ 4/5 - 3/7"  # 加括号后为 (1 )-( 0 )÷( 4/5 )-( 3/7)
+        exp2 = "7 + 2 - 4 ÷ 3/7"  # 加括号后为 (7 )+( 2 )-( 4 )÷( 3/7)
+        exp3 = "1/4 ÷ 2 - 6/7"  # 加括号后为 (1/4 )÷( 2 )-( 6/7)
+        exp4 = "4/5 × 6 + 1/3"  # 加括号后为 (4/5 )×( 6 )+( 1/3)
+        exp5 = "2 × 5/8"  # 加括号后为 (2 )×( 5/8)
+
+        pare_exp1 = "(1 )-( 0 )÷( 4/5 )-( 3/7)"
+        pare_exp2 = "(7 )+( 2 )-( 4 )÷( 3/7)"
+        pare_exp3 = "(1/4 )÷( 2 )-( 6/7)"
+        pare_exp4 = "(4/5 )×( 6 )+( 1/3)"
+        pare_exp5 = "(2 )×( 5/8)"
+
+        if add_parentheses(exp1) == pare_exp1 and add_parentheses(exp2) == pare_exp2 and add_parentheses(exp3) == pare_exp3 and add_parentheses(exp4) == pare_exp4 and add_parentheses(exp5) == pare_exp5:
+            self.assertTrue(1)
+        else:
+            self.assertFalse(0)
 
     def test_grade_questions(self):
         # 测试grade_questions函数是否正确统计正确和错误的题目
         correct_indices, wrong_indices = grade_questions("test_exercises.txt", "test_answers.txt")
-        self.assertEqual(len(correct_indices), 7)
-        self.assertEqual(len(wrong_indices), 3)
+        self.assertEqual(len(correct_indices), 5)
+        self.assertEqual(len(wrong_indices), 5)
 
     # 异常处理
     def test_invalid_file_paths(self):
@@ -54,7 +84,7 @@ class TestArithmeticGenerator(unittest.TestCase):
     def test_generate_expression(self):
         # 测试generate_expression函数生成的表达式是否合法
         for _ in range(100):
-            expression = generate_expression(10)
+            expression, expression_1 = generate_expression(10)
             if Fraction(eval(expression.replace('÷', '/').replace('×', '*'))) == ZeroDivisionError:
                 self.assertFalse(0)
             else:
